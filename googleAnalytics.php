f374caacf62d624e56da052da5504bc8e7bd4405
Tim Laqua <tlaqua@users.mediawiki.org>
2008-01-02 21:17:38 +0000

    * Removed backwards compatibility * Switched variable names - see new installation instructions on mw.org * Added $wgGoogleAnalyticsIgnoreSysops * Added $wgGoogleAnalyticsIgnoreBots * Initialized all variables properly

--- googleAnalytics.php
+++ googleAnalytics.php
@@ -8,53 +8,38 @@ $wgExtensionCredits['other'][] = array(
     'url'=>'http://www.mediawiki.org/wiki/Extension:Google_Analytics_Integration',
     'author'=>'Tim Laqua',
     'description'=>'Inserts Google Analytics script (ga.js) in to MediaWiki pages for tracking.',
-    'version'=>'1.3'
+    'version'=>'2.0'
 );
 
-if( version_compare( $wgVersion, '1.11alpha', '>=' ) ) {
-    $wgHooks['SkinAfterBottomScripts'][]  = 'efGoogleAnalyticsHookText'; 
-} else {
-	$wgHooks['MonoBookTemplateToolboxEnd'][]  = 'efGoogleAnalyticsHookEcho'; 
-	$wgHooks['BeforePageDisplay'][]  = 'efGoogleAnalyticsHookOut'; 
-}
+$wgHooks['SkinAfterBottomScripts'][]  = 'efGoogleAnalyticsHookText'; 
+
+$wgGoogleAnalyticsAccount = "";
+$wgGoogleAnalyticsIgnoreSysops = true;
+$wgGoogleAnalyticsIgnoreBots = true;
 
 function efGoogleAnalyticsHookText(&$skin, &$text='') {
 	$text .= efAddGoogleAnalytics();
 	return true;
 }
 
-function efGoogleAnalyticsHookEcho(&$out) {
-	global $googleAnalyticsMonobook;
-	if ($googleAnalyticsMonobook)
-		echo(efAddGoogleAnalytics()); 
-	return true;
-}
-
-function efGoogleAnalyticsHookOut(&$out) {
-	global $googleAnalyticsMonobook;
-	if (!$googleAnalyticsMonobook)
-		$out->addHTML(efAddGoogleAnalytics());
-	return true;
-}
-
 function efAddGoogleAnalytics() {
-    global $googleAnalytics, $wgUser;
-    if (!$wgUser->isAllowed('bot')) {
-        if (!$wgUser->isAllowed('protect')) {
-            if ($googleAnalytics) {
+    global $wgGoogleAnalyticsAccount, $wgGoogleAnalyticsIgnoreSysops, $wgGoogleAnalyticsIgnoreBots, $wgUser;
+    if (!$wgUser->isAllowed('bot') || !$wgGoogleAnalyticsIgnoreBots) {
+        if (!$wgUser->isAllowed('protect') || !$wgGoogleAnalyticsIgnoreSysops) {
+            if ( !empty($wgGoogleAnalyticsAccount) ) {
                 $funcOutput = <<<GASCRIPT
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
 <script type="text/javascript">
-var pageTracker = _gat._getTracker("{$googleAnalytics}");
+var pageTracker = _gat._getTracker("{$wgGoogleAnalyticsAccount}");
 pageTracker._initData();
 pageTracker._trackPageview();
 </script>
 GASCRIPT;
             } else {
-                    $funcOutput = "\n<!-- Set \$googleAnalytics to your uacct # provided by Google Analytics. -->";
+                    $funcOutput = "\n<!-- Set \$wgGoogleAnalyticsAccount to your account # provided by Google Analytics. -->";
             }
         } else {
             $funcOutput = "\n<!-- Google Analytics tracking is disabled for users with 'protect' rights (I.E. sysops) -->";
