d981c6b59ef4b7a7ff788c8732b5768266e1018d
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-20 11:28:16 +0000

    * add descriptionmsg for CrowdAuthentication, EditOwn, GlobalUsage, googleAnalytics * add support for CrowdAuthentication, EditOwn, GlobalUsage, googleAnalytics, and RandomImage to Translate

---
+++ googleAnalytics.i18n.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Internationalisation file for extension googleAnalytics.
+ *
+ * @addtogroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Tim Laqua
+ */
+$messages['en'] = array(
+	'googleanalytics-desc' => 'Inserts Google Analytics script (ga.js) in to MediaWiki pages for tracking',
+);
--- googleAnalytics.php
+++ googleAnalytics.php
@@ -1,17 +1,20 @@
 <?php
 if ( !defined( 'MEDIAWIKI' ) ) {
-        die( 'This file is a MediaWiki extension, it is not a valid entry point' );
+	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
 }
 
 $wgExtensionCredits['other'][] = array(
-    'name'=>'Google Analytics Integration',
-    'url'=>'http://www.mediawiki.org/wiki/Extension:Google_Analytics_Integration',
-    'author'=>'Tim Laqua',
-    'description'=>'Inserts Google Analytics script (ga.js) in to MediaWiki pages for tracking.',
-    'version'=>'2.0'
+	'name'           => 'Google Analytics Integration',
+	'version'        => '2.0.1'
+	'author'         => 'Tim Laqua',
+	'description'    => 'Inserts Google Analytics script (ga.js) in to MediaWiki pages for tracking.',
+	'descriptionurl' => 'googleanalytics-desc',
+	'url'            => 'http://www.mediawiki.org/wiki/Extension:Google_Analytics_Integration',
 );
 
-$wgHooks['SkinAfterBottomScripts'][]  = 'efGoogleAnalyticsHookText'; 
+$wgExtensionMessagesFiles['googleAnalytics'] = dirname(__FILE__) . '/googleAnalytics.i18n.php';
+
+$wgHooks['SkinAfterBottomScripts'][]  = 'efGoogleAnalyticsHookText';
 
 $wgGoogleAnalyticsAccount = "";
 $wgGoogleAnalyticsIgnoreSysops = true;
@@ -23,11 +26,11 @@ function efGoogleAnalyticsHookText(&$skin, &$text='') {
 }
 
 function efAddGoogleAnalytics() {
-    global $wgGoogleAnalyticsAccount, $wgGoogleAnalyticsIgnoreSysops, $wgGoogleAnalyticsIgnoreBots, $wgUser;
-    if (!$wgUser->isAllowed('bot') || !$wgGoogleAnalyticsIgnoreBots) {
-        if (!$wgUser->isAllowed('protect') || !$wgGoogleAnalyticsIgnoreSysops) {
-            if ( !empty($wgGoogleAnalyticsAccount) ) {
-                $funcOutput = <<<GASCRIPT
+	global $wgGoogleAnalyticsAccount, $wgGoogleAnalyticsIgnoreSysops, $wgGoogleAnalyticsIgnoreBots, $wgUser;
+	if (!$wgUser->isAllowed('bot') || !$wgGoogleAnalyticsIgnoreBots) {
+		if (!$wgUser->isAllowed('protect') || !$wgGoogleAnalyticsIgnoreSysops) {
+			if ( !empty($wgGoogleAnalyticsAccount) ) {
+				$funcOutput = <<<GASCRIPT
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
@@ -38,16 +41,16 @@ pageTracker._initData();
 pageTracker._trackPageview();
 </script>
 GASCRIPT;
-            } else {
-                    $funcOutput = "\n<!-- Set \$wgGoogleAnalyticsAccount to your account # provided by Google Analytics. -->";
-            }
-        } else {
-            $funcOutput = "\n<!-- Google Analytics tracking is disabled for users with 'protect' rights (I.E. sysops) -->";
-        }
-    } else {
-        $funcOutput = "\n<!-- Google Analytics tracking is disabled for bots -->";
-    }
- 
+			} else {
+				$funcOutput = "\n<!-- Set \$wgGoogleAnalyticsAccount to your account # provided by Google Analytics. -->";
+			}
+		} else {
+			$funcOutput = "\n<!-- Google Analytics tracking is disabled for users with 'protect' rights (I.E. sysops) -->";
+		}
+	} else {
+		$funcOutput = "\n<!-- Google Analytics tracking is disabled for bots -->";
+	}
+
 	return $funcOutput;
 }
 
