45bf63088869aac0532ba63757566b9f24b09b7f
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-20 15:37:10 +0000

    Localisation updates for extension messages from Betawiki (2008-02-10 16:10 CET)

--- googleAnalytics.i18n.php
+++ googleAnalytics.i18n.php
@@ -13,3 +13,25 @@ $messages = array();
 $messages['en'] = array(
 	'googleanalytics-desc' => 'Inserts Google Analytics script (ga.js) in to MediaWiki pages for tracking',
 );
+
+/** French (Français)
+ * @author Grondin
+ */
+$messages['fr'] = array(
+	'googleanalytics-desc' => 'Ajoute un script Google Analytique (ga.js) dans les pages MediaWiki pour les recherches.',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'googleanalytics-desc' => 'Zasunje skript Google Analytics (ga.js) do stronow MediaWiki za wuslědźenje',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'googleanalytics-desc' => "Voegt het script voor Google Analytics (ga.js) toe in MediaWiki pagina's voor tracking",
+);
+
