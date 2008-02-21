dc1bc20c9d98de3dd8dc98b31274f11222068de0
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-21 20:36:09 +0000

    Localisation updates for extension messages from Betawiki (2008-02-21 21:06 CET)

--- googleAnalytics.i18n.php
+++ googleAnalytics.i18n.php
@@ -14,6 +14,20 @@ $messages['en'] = array(
 	'googleanalytics-desc' => 'Inserts Google Analytics script (ga.js) in to MediaWiki pages for tracking',
 );
 
+/** Arabic (العربية)
+ * @author Meno25
+ */
+$messages['ar'] = array(
+	'googleanalytics-desc' => 'يضيف سكريبت جوجل التحليلي (ga.js) إلى صفحات الميدياويكي للتتبع',
+);
+
+/** Bulgarian (Български)
+ * @author DCLXVI
+ */
+$messages['bg'] = array(
+	'googleanalytics-desc' => 'Вмъква скрипт на Google Analytics (ga.js) в страниците на МедияУики',
+);
+
 /** German (Deutsch)
  * @author Raimond Spekking
  */
@@ -37,8 +51,30 @@ $messages['hsb'] = array(
 
 /** Dutch (Nederlands)
  * @author Siebrand
+ * @author SPQRobin
  */
 $messages['nl'] = array(
-	'googleanalytics-desc' => "Voegt het script voor Google Analytics (ga.js) toe in MediaWiki pagina's voor tracking",
+	'googleanalytics-desc' => "Voegt het script voor Google Analytics (ga.js) toe in MediaWiki-pagina's voor tracking",
+);
+
+/** Norwegian (‪Norsk (bokmål)‬)
+ * @author Jon Harald Søby
+ */
+$messages['no'] = array(
+	'googleanalytics-desc' => 'Setter inn Google Analytics-skrift (ga.js)  på MediaWiki-sider for sporing',
+);
+
+/** Slovak (Slovenčina)
+ * @author Helix84
+ */
+$messages['sk'] = array(
+	'googleanalytics-desc' => 'Vkladá sledovací skript Google Analytics (ga.js) do stránok MediaWiki',
+);
+
+/** Vietnamese (Tiếng Việt)
+ * @author Vinhtantran
+ */
+$messages['vi'] = array(
+	'googleanalytics-desc' => 'Chèn mã Google Analytics (ga.js) vào các trang MediaWiki để dò tìm',
 );
 
